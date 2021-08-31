ae13a61a0632b47d574f6e985a05052f2d6a3fca
Roland Leißa <leissa@cs.uni-saarland.de>
2021-08-31 03:40:00 +0200

    wip: capture-avoiding subst

--- lam.cpp
+++ lam.cpp
@@ -3,14 +3,11 @@
 #include <string>
 #include <unordered_map>
 
-/*
- * e := x
- *   | e e
- *   | lam x. exp
- */
+template<class T>
+using Ptr = std::unique_ptr<T>;
 
-class Exp;
-using Ctxt = std::unordered_map<std::string, std::unique_ptr<Exp>>;
+template<class T, class... Args>
+Ptr<T> mk(Args&&... args) { return std::make_unique<T>(std::forward<Args>(args)...); }
 
 class Exp {
 public:
@@ -20,66 +17,26 @@ public:
         stream(std::cout) << std::endl;
     }
 
-    std::unique_ptr<Exp> run() const {
-        Ctxt ctxt;
-        return eval(ctxt);
-    }
-
+    virtual Ptr<Exp> clone() const = 0;
     virtual std::ostream& stream(std::ostream&) const = 0;
-    virtual std::unique_ptr<Exp> clone() const = 0;
-    virtual std::unique_ptr<Exp> eval(Ctxt&) const = 0;
-};
-
-class Lam : public Exp {
-public:
-    Lam(std::string&& binder, std::unique_ptr<Exp>&& body)
-        : binder_(std::move(binder))
-        , body_(std::move(body))
-    {}
-
-    const std::string& binder() const { return binder_; }
-    const Exp* body() const { return body_.get(); }
-
-    std::ostream& stream(std::ostream& o) const override {
-        o << "(λ " << binder() << ". ";
-        return body()->stream(o) << ")";
-    }
-
-    std::unique_ptr<Exp> clone() const override {
-        return std::make_unique<Lam>(std::string(binder()), body()->clone());
-    }
-
-    std::unique_ptr<Exp> eval(Ctxt& ctxt) const override {
-        return std::make_unique<Lam>(std::string(binder()), body()->eval(ctxt));
-    }
-
-private:
-    static int fresh_ = 0;
-
-    std::string binder_;
-    std::unique_ptr<Exp> body_;
+    virtual Ptr<Exp> rename(const std::string&, const std::string&) const = 0;
+    virtual Ptr<Exp> subst(const std::string&, const Exp&) const = 0;
+    virtual Ptr<Exp> eval() const = 0;
 };
 
 class Var : public Exp {
 public:
-    Var(std::string&& name)
+    Var(const std::string& name)
         : name_(std::move(name))
     {}
 
     const std::string& name() const { return name_; }
 
-    std::ostream& stream(std::ostream& o) const override {
-        return o << name();
-    }
-
-    std::unique_ptr<Exp> clone() const override {
-        return std::make_unique<Var>(std::string(name()));
-    }
-
-    std::unique_ptr<Exp> eval(Ctxt& ctxt) const override {
-        auto i = ctxt.find(name());
-        return i != ctxt.end() ? i->second->clone() : clone();
-    }
+    Ptr<Exp> clone() const override { return mk<Var>(name()); }
+    Ptr<Exp> rename(const std::string& x, const std::string& y) const override;
+    Ptr<Exp> subst(const std::string& x, const Exp& e) const;
+    Ptr<Exp> eval() const override;
+    std::ostream& stream(std::ostream& o) const override;
 
 private:
     std::string name_;
@@ -87,7 +44,7 @@ private:
 
 class App : public Exp {
 public:
-    App(std::unique_ptr<Exp>&& callee, std::unique_ptr<Exp>&& arg)
+    App(Ptr<Exp>&& callee, Ptr<Exp>&& arg)
         : callee_(std::move(callee))
         , arg_(std::move(arg))
     {}
@@ -95,42 +52,124 @@ public:
     const Exp* callee() const { return callee_.get(); }
     const Exp* arg() const { return arg_.get(); }
 
-    std::ostream& stream(std::ostream& o) const override {
-        o << "(";
-        callee()->stream(o) << " ";
-        return arg()->stream(o) << ")";
-    }
+    Ptr<Exp> clone() const override { return mk<App>(callee()->clone(), arg()->clone()); }
+    Ptr<Exp> rename(const std::string&, const std::string& ) const override;
+    Ptr<Exp> subst(const std::string&, const Exp&) const override;
+    Ptr<Exp> eval() const override;
+    std::ostream& stream(std::ostream&) const override;
 
-    std::unique_ptr<Exp> clone() const override {
-        return std::make_unique<App>(callee()->clone(), arg()->clone());
-    }
-
-    std::unique_ptr<Exp> eval(Ctxt& ctxt) const override {
-        auto c = callee()->eval(ctxt);
-        auto a = arg()->eval(ctxt);
-
-        if (auto lam = dynamic_cast<const Lam*>(c.get())) {
-            ctxt[lam->binder()] = std::move(a);
-            return lam->body()->eval(ctxt);
-        }
-
-        return std::make_unique<App>(std::move(c), std::move(a));
-    }
 private:
-    std::unique_ptr<Exp> callee_;
-    std::unique_ptr<Exp> arg_;
+    Ptr<Exp> callee_;
+    Ptr<Exp> arg_;
 };
 
-auto var(std::string&& x) { return std::make_unique<Var>(std::move(x)); }
-auto app(std::unique_ptr<Exp>&& callee, std::unique_ptr<Exp>&& arg) { return std::make_unique<App>(std::move(callee), std::move(arg)); }
-auto lam(std::string&& x, std::unique_ptr<Exp>&& body) { return std::make_unique<Lam>(std::move(x), std::move(body)); }
+class Lam : public Exp {
+public:
+    Lam(const std::string& binder, Ptr<Exp>&& body)
+        : binder_(std::move(binder))
+        , body_(std::move(body))
+    {}
+
+    const std::string& binder() const { return binder_; }
+    const Exp* body() const { return body_.get(); }
+
+    Ptr<Exp> clone() const override { return mk<Lam>(binder(), body()->clone()); }
+    Ptr<Exp> rename(const std::string& x, const std::string& y) const override;
+    Ptr<Exp> subst(const std::string& x, const Exp& e) const override;
+    Ptr<Exp> eval() const override;
+    std::ostream& stream(std::ostream& o) const override;
+
+private:
+    static int counter_;
+
+    std::string binder_;
+    Ptr<Exp> body_;
+};
+
+int Lam::counter_ = 0;
+
+// rename
+
+Ptr<Exp> Var::rename(const std::string& x, const std::string& y) const {
+    return name() == x ? mk<Var>(y) : clone();
+}
+
+Ptr<Exp> App::rename(const std::string& x, const std::string& y) const {
+    auto c = callee()->rename(x, y);
+    auto a = arg()->rename(x, y);
+    return mk<App>(std::move(c), std::move(a));
+}
+
+Ptr<Exp> Lam::rename(const std::string& x, const std::string& y) const {
+    return mk<Lam>(binder() == x ? y : binder(), body()->rename(x, y));
+}
+
+// subst
+
+Ptr<Exp> Var::subst(const std::string& x, const Exp& e) const {
+    return name() == x ? e.clone() : clone();
+}
+
+Ptr<Exp> App::subst(const std::string& x, const Exp& e) const {
+    auto c = callee()->subst(x, e);
+    auto a = arg()->subst(x, e);
+    return mk<App>(std::move(c), std::move(a));
+}
+
+Ptr<Exp> Lam::subst(const std::string& x, const Exp& e) const {
+    if (binder() == x) return body()->clone();
+
+    auto fresh = binder() + "_" + std::to_string(counter_++);
+    auto b = body()->rename(binder(), fresh);
+    return mk<Lam>(fresh, b->subst(x, e));
+}
+
+// eval
+
+Ptr<Exp> Var::eval() const {
+    return clone();
+}
+
+Ptr<Exp> App::eval() const {
+    auto c = callee()->eval();
+    auto a = arg()->eval();
+
+    if (auto lam = dynamic_cast<const Lam*>(c.get()))
+        return lam->body()->subst(lam->binder(), *a);
+
+    return mk<App>(std::move(c), std::move(a));
+}
+
+Ptr<Exp> Lam::eval() const {
+    return mk<Lam>(binder(), body()->eval());
+}
+
+// stream
+
+std::ostream& Var::stream(std::ostream& o) const {
+    return o << name();
+}
+
+
+std::ostream& App::stream(std::ostream& o) const {
+    o << "(";
+    callee()->stream(o) << " ";
+    return arg()->stream(o) << ")";
+}
+
+std::ostream& Lam::stream(std::ostream& o) const {
+    o << "(λ " << binder() << ". ";
+    return body()->stream(o) << ")";
+}
+
+// driver
 
 int main() {
-    auto p = var("x");
-    auto x = p->run();
+    auto p = mk<Var>("x");
+    auto x = p->eval();
     //x->dump();
     // (lam x. x) z
-    auto p1 = app(lam("x", var("x")), var("z"));
+    auto p1 = mk<App>(mk<Lam>("x", mk<Var>("x")), mk<Var>("z"));
     p1->dump();
 
     auto p2 = p1->clone();
@@ -139,24 +178,24 @@ int main() {
     p2->dump();
 
     // λf. λx. x
-    auto n0 = lam("f0", lam("x0", var("x0")));
+    auto n0 = mk<Lam>("f", mk<Lam>("x", mk<Var>("x")));
     // λf. λx. f x
-    auto n1 = lam("f1", lam("x1", app(var("f1"), var("x1"))));
+    auto n1 = mk<Lam>("f", mk<Lam>("x", mk<App>(mk<Var>("f"), mk<Var>("x"))));
     // λf. λx. f (f x)
-    auto n2 = lam("f2", lam("x2", app(var("f2"), app(var("f2"), var("x2")))));
+    auto n2 = mk<Lam>("f", mk<Lam>("x", mk<App>(mk<Var>("f"), mk<App>(mk<Var>("f"), mk<Var>("x")))));
 
     n0->dump();
     n1->dump();
     n2->dump();
 
     // λn. λf. λx. f (n f x)
-    auto succ = lam("n", lam("fs", lam("xs", app(var("fs"), app(app(var("n"), var("fs")), var("xs"))))));
+    auto succ = mk<Lam>("n", mk<Lam>("f", mk<Lam>("x", mk<App>(mk<Var>("f"), mk<App>(mk<App>(mk<Var>("n"), mk<Var>("f")), mk<Var>("x"))))));
     succ->dump();
 
-    app(succ->clone(), n1->clone())->run()->dump();
+    mk<App>(succ->clone(), n1->clone())->eval()->dump();
     n2->dump();
 
-    auto p3 = p2->run();
+    auto p3 = p2->eval();
     //p3->dump();
 
     return 0;
