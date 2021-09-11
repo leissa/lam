085cb877ff8381e0496671f744500af65f355312
Roland Leißa <leissa@uni-mannheim.de>
2021-09-11 06:06:08 +0200

    added support for stdin and --version

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,13 @@ static const auto usage =
 "\n"
 "Options:\n"
 "\t-h, --help\tdisplay this help and exit\n"
+"\t-v, --version\tdisplay version info and exit\n"
 "\t-e, --eval\tevaluate program before dumping\n"
+"\n"
+"Hint: use '-' as file to read from stdin.\n"
 ;
+
+static const auto version = "lam interpreter 0.1\n";
 
 int main(int argc, char** argv) {
     try {
@@ -22,6 +27,9 @@ int main(int argc, char** argv) {
         for (int i = 1; i != argc; ++i) {
             if (strcmp("-h", argv[i]) == 0 || strcmp("--help", argv[i]) == 0) {
                 std::cerr << usage;
+                return EXIT_SUCCESS;
+            } else if (strcmp("-v", argv[i]) == 0 || strcmp("--version", argv[i]) == 0) {
+                std::cerr << version;
                 return EXIT_SUCCESS;
             } else if (strcmp("-e", argv[i]) == 0 || strcmp("--eval", argv[i]) == 0) {
                 eval = true;
@@ -35,12 +43,19 @@ int main(int argc, char** argv) {
         if (file == nullptr)
             throw std::logic_error("no input file given");
 
-        std::ifstream ifs(file);
-        Parser parser(file, ifs);
-        auto exp = parser.parse_prg();
+        Ptr<Exp> exp;
+        if (strcmp("-", file) == 0) {
+            Parser parser("<stdin>", std::cin);
+            exp = parser.parse_prg();
+        } else {
+            std::ifstream ifs(file);
+            Parser parser(file, ifs);
+            exp = parser.parse_prg();
+        }
+
         if (eval) exp = exp->eval();
         exp->dump();
-    } catch (std::exception const& e) {
+    } catch (const std::exception& e) {
         std::cerr << "error: " << e.what() << std::endl;
         std::cerr << usage;
         return EXIT_FAILURE;
